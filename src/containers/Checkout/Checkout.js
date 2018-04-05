78772bbb295c9ef6a914d8c909ffcfed8f3070b4
Daniel Joyce <Naiyon@users.noreply.github.com>
2018-04-05 21:07:13 +0100

    Redirecting after ordering burger

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -4,8 +4,11 @@ import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
+import * as actions from '../../store/actions/';
 
 class Checkout extends Component {
+
+
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
@@ -17,10 +20,14 @@ class Checkout extends Component {
 
     render () {
         let summary = <Redirect to ="/"/>
-
+        
         if(this.props.ings){
+
+            const purchasedRedirect = this.props.purchased ? <Redirect to="/"/> : null
+
             summary = (
                 <div>
+                    {purchasedRedirect}
                     <CheckoutSummary
                     ingredients={this.props.ings}
                     checkoutCancelled={this.checkoutCancelledHandler}
@@ -38,8 +45,10 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased:state.order.purchased
     }
 };
+
 
 export default connect(mapStateToProps)(Checkout);
